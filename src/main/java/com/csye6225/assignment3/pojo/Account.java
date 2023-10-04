97351071b9321974be3aecb7f088279ed21f5cc1
Swetha Paturu <paturu.s@northeastern.edu>
2023-10-04 19:18:13 -0400

    basic authentication (#9)

--- src/main/java/com/csye6225/assignment3/pojo/Account.java
+++ src/main/java/com/csye6225/assignment3/pojo/Account.java
@@ -1,12 +1,16 @@
 package com.csye6225.assignment3.pojo;
 
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -48,6 +52,9 @@ public class Account {
 	    this.account_created = new Date();
 	    this.account_updated = this.account_created; 
 	}
+	
+	@OneToMany(mappedBy = "account", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	 private List<Assignment> assignments;
 
 	public int getId() {
 		return id;
@@ -82,6 +89,19 @@ public class Account {
 		return email;
 	}
 
+
+	public List<Assignment> getAssignments() {
+		return assignments;
+	}
+
+	public void setAssignments(List<Assignment> assignments) {
+		this.assignments = assignments;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -101,7 +121,6 @@ public class Account {
 	public void setAccount_updated(Date account_updated) {
 		this.account_updated = account_updated;
 	}
-	
 }
 
 
