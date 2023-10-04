623074c0c4419703e9c5cc3766d40de48e4db75b
Swetha Paturu <paturu.s@northeastern.edu>
2023-10-03 20:32:02 -0400

    no authentication for /healthz (#3)

--- src/main/java/com/csye6225/assignment3/webapp/WebappApplication.java
+++ src/main/java/com/csye6225/assignment3/webapp/WebappApplication.java
@@ -2,8 +2,12 @@ package com.csye6225.assignment3.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.csye6225.assignment3.pojo")
+@ComponentScan(basePackages = {"com.csye6225.assignment3.security","com.csye6225.assignment3.controllers"})
 public class WebappApplication {
 
 	public static void main(String[] args) {
