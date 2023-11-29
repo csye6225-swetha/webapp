c60b39d50195c7fe6f5367cb6a19b62a34e5b60a
Swetha Paturu <paturu.s@northeastern.edu>
2023-11-28 20:51:14 -0500

    Working sns (#97)
    * Delete .github/workflows directory
    
    * sns service
    
    * sns
    
    * troubleshooting
    
    * Update ci.yaml
    
    * Update application.properties
    
    * Update ci.yaml

--- src/main/java/com/csye6225/assignment3/services/SnsService.java
+++ src/main/java/com/csye6225/assignment3/services/SnsService.java
@@ -1,11 +1,17 @@
 package com.csye6225.assignment3.services;
 
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
 import software.amazon.awssdk.services.sns.model.PublishResponse;
+import software.amazon.awssdk.services.sns.model.SnsException;
 
 @Service
 public class SnsService {
@@ -13,25 +19,36 @@ public class SnsService {
 	private final SnsClient snsClient;
 	private final String topicArn;
 	
-	
-	public SnsService(@Value("${sns.topic.arn:}") String topicArn) {
+
+	public SnsService(@Value("${sns.topic.arn}") String topicArn) {
         this.snsClient = SnsClient.builder().build();
         this.topicArn = topicArn;
     }
+	
+	private static final Logger logger = LoggerFactory.getLogger(SnsService.class);
 
-    public String publishMessage(String message) {
-        PublishRequest request = PublishRequest.builder()
-                                               .topicArn(topicArn)
-                                               .message(message)
-                                               .build();
-        PublishResponse response = snsClient.publish(request);
-        return response.messageId();
+	public String publishMessage(String message) {
+        try {
+            PublishRequest request = PublishRequest.builder()
+                                                   .topicArn(topicArn)
+                                                   .message(message)
+                                                   .build();
+            PublishResponse response = snsClient.publish(request);
+            return response.messageId();
+        } catch (SnsException e) {
+            logger.error("Error while publishing message to SNS: " + e.awsErrorDetails().errorMessage(), e);
+            return null; // Or handle the exception as needed
+        }
     }
-    
-    
+
     public void notifySubmission(String email, String submissionUrl) {
         String message = "Submission received from " + email + " with URL: " + submissionUrl;
-        publishMessage(message); // Using the existing publishMessage method
+        String messageId = publishMessage(message);
+        System.out.println("MessageId:" + messageId);
+        if (messageId == null) {
+            logger.error("Failed to send notification for submission from " + email);
+        }
     }
+                                         
 
 }
